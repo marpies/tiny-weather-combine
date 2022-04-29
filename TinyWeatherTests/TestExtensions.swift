31220ebe5a2804642cb3af9aa6b0147860c08737
Marcel Piešťanský <marcel@piestansky.cz>
2022-04-29 13:08:20 +0200

    Updated test extensions

--- TinyWeatherTests/TestExtensions.swift
+++ TinyWeatherTests/TestExtensions.swift
@@ -10,6 +10,8 @@
 //  
 
 import Foundation
+import XCTest
+import Combine
 
 extension Dictionary where Key == String, Value == Any {
     
@@ -39,6 +41,50 @@ extension Array where Element == [String: Any] {
     
     var asData: Data {
         return try! JSONSerialization.data(withJSONObject: self, options: [])
+    }
+    
+}
+
+extension XCTestCase {
+    
+    /// https://www.swiftbysundell.com/articles/unit-testing-combine-based-swift-code/
+    func awaitPublisher<T: Publisher>(
+        _ publisher: T,
+        timeout: TimeInterval = 0.5,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> Result<T.Output, Error> {
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting publisher")
+        
+        let cancellable = publisher.sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    result = .failure(error)
+                case .finished:
+                    break
+                }
+                
+                expectation.fulfill()
+            },
+            receiveValue: { value in
+                result = .success(value)
+            }
+        )
+        
+        waitForExpectations(timeout: timeout)
+        
+        cancellable.cancel()
+        
+        let unwrappedResult = try XCTUnwrap(
+            result,
+            "Awaited publisher did not produce any output",
+            file: file,
+            line: line
+        )
+        
+        return unwrappedResult
     }
     
 }
